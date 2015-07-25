bb1333922184370026fa611b152072b46f08ef61
Mikita Vorobyov <olfb_c@ukr.net>
2015-07-25 16:59:27 +0300

    переименование откладывается чтобы не копаться

--- fitness_test/wsgi.py
+++ fitness_test/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for polyclinic project.
+WSGI config for fitness_test project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "polyclinic.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fitness_test.settings")
 
 application = get_wsgi_application()
